2ca308d839a488dd428acc291e2dad384c3a0383
Hemcode <pro.hemdy@gmail.com>
2023-03-18 17:23:38 +0100

    Version6

---
+++ script.js
@@ -0,0 +1,19 @@
+const main = () => {
+    const body = document.querySelector("body");
+
+    const h1 = document.querySelector("h1");
+    h1.style.textAlign = "center";
+
+    const h2 = document.querySelector("h2");
+    h2.style.textAlign = "center";
+
+    const codeWriterTitle = document.getElementById("code_writer");
+    codeWriterTitle.style.textAlign = "center";
+
+    const oPreject = document.getElementById("oProject");
+    oPreject.style.textAlign = "center";
+}
+
+
+if ("__main__" === "__main__")
+    main()
